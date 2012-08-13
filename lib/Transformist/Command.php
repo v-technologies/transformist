2918dadcb25949b09495d3b33dcd4b5f9ebfc2a4
Félix Girault <felix.girault@gmail.com>
2012-08-13 12:29:27 +0200

    Added commands logging

--- lib/Transformist/Command.php
+++ lib/Transformist/Command.php
@@ -10,26 +10,73 @@
 class Transformist_Command {
 
 	/**
+	 *	An array of all command that are executed.
+	 *
+	 *	@var array
+	 */
+
+	protected static $_commands = array( );
+
+
+
+	/**
 	 *	Constructor.
 	 *
 	 *	@param string $name Command name.
-	 *	@param array $arguments Command arguments.
+	 *	@param array $options Command options.
+	 *	@param string $assignment An assignment character that will be used to
+	 *		associate long options and their values. It is generally a space
+	 *		or an equals sign.
 	 */
 
-	public static function execute( $name, $arguments = array( )) {
+	public static function execute( $name, $options = array( ), $assignment = ' ' ) {
 
 		$command = $name;
 
-		foreach ( $arguments as $key => $value ) {
+		foreach ( $options as $key => $value ) {
 			$command .= ' ';
 
 			if ( is_string( $key )) {
-				$command .= $key . ' ';
+				$command .= $key . $assignment;
+				$value = escapeshellarg( $value );
 			}
 
 			$command .= $value;
 		}
 
-		return exec( $command );
+		self::$_commands[] = $command;
+		return @exec( $command );
+	}
+
+
+
+	/**
+	 *	Returns all the executed commands.
+	 *
+	 *	@return array
+	 */
+
+	public static function executed( ) {
+
+		return self::$_commands;
+	}
+
+
+
+	/**
+	 *	Returns the last executed command.
+	 *
+	 *	@return string|false The last command, or false if none were already
+	 *		executed.
+	 */
+
+	public static function last( ) {
+
+		if ( empty( self::$_commands )) {
+			return false;
+		}
+
+		$last = count( self::$_commands ) - 1;
+		return self::$_commands[ $last ];
 	}
 }
